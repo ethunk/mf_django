540b3b3f01e06e50f0065506fa458fc08b22427d
Eric Hunkler <ethunk@gmail.com>
2021-01-27 19:57:22 -0700

    add readme, requirements

---
+++ README.md
@@ -0,0 +1,8 @@
+## Project Info
+
+## Requirements to run
+* Recommend creating a local virtual environment.
+* If you use asdf as a language manager, you can `asdf install` which should grab the python version specified in the `.tool-versions`
+1.  `pip install -r ./requirements.txt`
+2.  `python manage.py runserver_plus`
+3.  Navigate to 127.0.0.1:8000
--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^khabqzk51fpsv2*h@t*n44d3#^%$+6t=ct13cedq8t947ek_9'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
