c710a3ddb08132bcfc2ddebbf21718a2158a7466
Eric <ethunk@gmail.com>
2021-01-31 16:04:24 -0700

    added redirect, added a tag to more elements

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from fool import views
 
 urlpatterns = [
     path('fool/', include('fool.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index),
 ]
