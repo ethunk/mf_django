7f3c397615c550940cb8aee72032f0648f9b6089
Eric <ethunk@gmail.com>
2021-01-31 16:27:54 -0700

    updated shuffle javascript to run recursively

--- fool/static/fool/js/main.js
+++ fool/static/fool/js/main.js
@@ -1,6 +1,11 @@
 function shuffleTickers() {
     var tickers = document.getElementsByClassName('ticker-row');
-    var element1 = Math.floor(Math.random()*6);
-    var element2 = Math.floor(Math.random()*6);
+    var element1 = Math.floor(Math.random()*tickers.length);
+    var element2 = Math.floor(Math.random()*tickers.length);
     tickers[element1].parentNode.insertBefore(tickers[element1], tickers[element2]);
+    console.log('Shuffled Once.')
+    if (element1 == element2) {
+        console.log('Shuffling again.')
+        shuffleTickers()
+    }
 }
